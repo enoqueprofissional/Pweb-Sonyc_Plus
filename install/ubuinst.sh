5fcc8fa051dde49251a63fcd3ce9650430d34777
lucianolima2905 <65465300+lucianolima2905@users.noreply.github.com>
2022-11-09 23:02:10 -0300

    Pweb-Sonyc_Plus

--- install/ubuinst.sh
+++ install/ubuinst.sh
@@ -138,7 +138,7 @@ cat /dev/null > ~/.bash_history && history -c
 rm /bin/ubuinst* > /dev/null 2>&1
 exit;
 else
-  echo -e 'by: @sonycssh >/usr/lib/telegram
+  echo -e 'by: @swittecnologia' >/usr/lib/telegram
   msg -bar
   echo -e "\e[1;97m           \e[5m\033[1;100m   ATUALIZAÇÃO DO SISTEMA   \033[1;37m"
   msg -bar
@@ -333,7 +333,7 @@ clear
 echo -e "\E[44;1;37m    INSTALANDO PAINEL    \E[0m"
 echo ""
 echo -e "WEB SWIT SSH" | figlet | boxes -d stone -p a0v0 | lolcat
-echo -e "                              \033[1;31mBy @sonycssh\033[1;36m" | lolcat
+echo -e "                              \033[1;31mBy @swittecnologia\033[1;36m" | lolcat
 echo ""
 chave=$(curl -sSL "https://github.com/lucianolima2905/Pweb-Sonyc_Plus/raw/main/install/chave") &>/dev/null
 
@@ -376,7 +376,7 @@ install_continue2
 } > /dev/null
 echo ""
 echo -e "WEB SWIT SSH" | figlet | boxes -d stone -p a0v0 | lolcat
-echo -e "                              \033[1;31mBy @sonycssh\033[1;36m" | lolcat
+echo -e "                              \033[1;31mBy @swittecnologia\033[1;36m" | lolcat
 echo ""
 echo -e "\033[1;36mDEFINA UMA NOVA SENHA PARA\033[0m" | lolcat
 echo -e "\033[1;36mO USUÁRIO ROOT DA VPS E\033[0m" | lolcat
@@ -398,7 +398,7 @@ clear
 sed -i "s;upload_max_filesize = 2M;upload_max_filesize = 256M;g" /etc/php/8.1/apache2/php.ini > /dev/null 2>&1
 sed -i "s;post_max_size = 8M;post_max_size = 256M;g" /etc/php/8.1/apache2/php.ini > /dev/null 2>&1
 echo -e "PAINEL WEB SWIT SSH" | figlet | boxes -d stone -p a0v0 | lolcat
-echo -e "                              \033[1;31mBy @sonycssh\033[1;36m" | lolcat
+echo -e "                              \033[1;31mBy @swittecnologia\033[1;36m" | lolcat
 echo ""
 echo -e "\033[1;32mPAINEL INSTALADO COM SUCESSO!" | lolcat
 echo ""
@@ -413,7 +413,7 @@ echo -e "\033[1;36m USUÁRIO:\033[1;37m root\033[0m"
 echo -e "\033[1;36m SENHA:\033[1;37m $pwdroot\033[0m"
 echo ""
 echo -e "\033[1;31m \033[1;33mCOMANDO PRINCIPAL: \033[1;32mpweb\033[0m"
-echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@sonycssh\033[0m" | lolcat
+echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@swittecnologia\033[0m" | lolcat
 echo ""
 echo -ne "\n\033[1;31mENTER \033[1;33mpara retornar...\033[1;32m! \033[0m"; read
 systemctl restart apache2 > /dev/null 2>&1
